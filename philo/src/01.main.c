d230b88bcdb0ea3698f4bff315fba4ac8dea07b9
biralavor <umeneses@student.42sp.org.br>
2024-10-17 17:11:48 -0300

    improving instructions feedback

--- philo/src/01.main.c
+++ philo/src/01.main.c
@@ -6,17 +6,48 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 16:03:55 by umeneses          #+#    #+#             */
-/*   Updated: 2024/10/17 16:32:28 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/10/17 16:53:20 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
 #include <stdio.h>
+#include <unistd.h>
+
+void	*routine (void *arg)
+{
+	(void)arg;
+	printf("Hello, from thread!\n");
+	sleep(3);
+	printf("Bye, from thread!\n");
+	return (NULL);
+}
 
 int main (void)
 {
-	printf("Hello, world!\n");
+	pthread_t	t01, t02;
 
+	if (pthread_create(&t01, NULL, &routine, NULL) != 0)
+	{
+		perror("pthread_create error on t01");
+		return (1);
+	}
+	if (pthread_create(&t02, NULL, &routine, NULL) != 0)
+	{
+		perror("pthread_create error on t02");
+		return (2);
+	}
+	if (pthread_join(t01, NULL) != 0)
+	{
+		perror("pthread_join error on t01");
+		return (3);
+	}
+	if (pthread_join(t02, NULL) != 0)
+	{
+		perror("pthread_join error on t02");
+		return (4);
+	}
+	printf(">>> Hello, world!\n");
 	return (0);
 }
