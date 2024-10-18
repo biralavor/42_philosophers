c31fb065eff2b3f54ee9fa650c991040ef06d0f1
biralavor <umeneses@student.42sp.org.br>
2024-10-18 12:12:49 -0300

    studies - sending an argument to routine() through pthread_create()

--- philo/src/01.main.c
+++ philo/src/01.main.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 16:03:55 by umeneses          #+#    #+#             */
-/*   Updated: 2024/10/18 10:17:28 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/10/18 12:11:11 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,19 @@
 void	*routine (void *arg)
 {
 	int	mails = 0;
-	int *result = malloc(sizeof(int));
-	(void)arg;
-	int idx = -1;
+	int *result = malloc(sizeof(int)); // it will be freed after pthread_join()
+	int idx = 0;
 	printf("Thread %ld has started with PID: %lu\n", pthread_self(), (unsigned long)pthread_self());
-	while (++idx < 1000000)
+	while (idx < 2)
 	{	
 		pthread_mutex_lock(&mutex);
 		mails++;
-		pthread_mutex_unlock(&mutex);	
+		mails = mails * (*(int *)arg);
+		pthread_mutex_unlock(&mutex);
+		idx++;	
 	}
 	*result = mails;
-	printf("func result pointer = %p\n", result);
+	free(arg);
 	return ((void *) result);
 }
 
@@ -46,23 +47,23 @@ int main (int ac, char **av)
 	pthread_mutex_init(&mutex, NULL);
 	while (++idx < pth_init)
 	{	
-		if (pthread_create(&pth[idx], NULL, &routine, NULL) != 0)
+		int	*arg_to_routine = malloc(sizeof(int)); // it will be freed inside routine called by pthread_create()
+		*arg_to_routine = idx;
+		if (pthread_create(&pth[idx], NULL, &routine, arg_to_routine) != 0)
 		{
-			perror("pthread_create error on t01");
+			perror("pthread_create error");
 			return (1);
 		}
 	}
 	idx = -1;
-	// Wait for all threads to finish
 	while (++idx < pth_init)
 	{	
 		if (pthread_join(pth[idx], (void **) &res) != 0)
 		{
-			perror("pthread_join error on t01");
+			perror("pthread_join error");
 			return (3);
 		}
 	}
-	printf("main result pointer = %p\n", res);
 	printf(">>> total mails from res = %d !\n", *res);
 	pthread_mutex_destroy(&mutex);
 	free(res);
