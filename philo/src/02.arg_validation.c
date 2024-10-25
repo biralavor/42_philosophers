869bc918535e486f2c5b24593ed084f3822ea977
biralavor <umeneses@student.42sp.org.br>
2024-10-25 20:30:15 -0300

    moving arg validation functions into a solo file

--- philo/src/02.arg_validation.c
+++ philo/src/02.arg_validation.c
@@ -1,33 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   02.parsing.c                                       :+:      :+:    :+:   */
+/*   02.arg_validation.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/22 16:16:16 by umeneses          #+#    #+#             */
-/*   Updated: 2024/10/25 19:30:15 by umeneses         ###   ########.fr       */
+/*   Created: 2024/10/25 20:24:28 by umeneses          #+#    #+#             */
+/*   Updated: 2024/10/25 20:28:09 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-t_table	*table_alloc(t_table *table, char **av)
-{
-	table = malloc(sizeof(t_table));
-	table->set = malloc(sizeof(t_set));
-	table->philo = malloc(sizeof(t_philo) * ft_atoi_long_int(av[1]));
-	table->philo->th_id = malloc(sizeof(pthread_t)  * ft_atoi_long_int(av[1]));
-	table->ph_mtx = malloc(sizeof(pthread_mutex_t));
-	return (table);
-}
-
-void	table_parsing(t_table *table, char **av)
-{
-	table = table_alloc(table, av);
-	table->set->total_philos = ft_atoi_long_int(av[1]);
-	table = table_holder(table, false);
-}
 
 bool	arguments_amount(int ac)
 {
@@ -41,13 +24,6 @@ bool	arguments_amount(int ac)
 	return (true);
 }
 
-bool	is_space(const char c)
-{
-	if ((c >= 9 && c <= 13) || c == 32)
-		return (true);
-	return (false);
-}
-
 bool	av_is_a_number(char **av)
 {
 	int	idx;
@@ -59,7 +35,7 @@ bool	av_is_a_number(char **av)
 		c = 0;
 		while(av[idx][c])
 		{
-			while (is_space(av[idx][c]))
+			while (ft_is_space(av[idx][c]))
 				c++;
 			if (av[idx][c] == '+' || av[idx][c] == '-')
 				c++;
