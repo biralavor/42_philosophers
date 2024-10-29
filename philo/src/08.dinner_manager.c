ffcdbe0b8812d7695fabd465626cab7470ee9635
biralavor <umeneses@student.42sp.org.br>
2024-10-29 10:15:11 -0300

    updating file names

---
+++ philo/src/08.dinner_manager.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   08.dinner_manager.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/29 10:01:45 by umeneses          #+#    #+#             */
+/*   Updated: 2024/10/29 10:14:17 by umeneses         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	dinner_manager(t_table *table)
+{
+	int	idx;
+
+	idx = -1;
+	if (table->set->total_meals == -1)
+	{
+		// TODO
+	}
+	else if (table->set->total_meals > 0)
+		return ; // back to main and clean
+	else if (table->set->total_meals == 1)
+	{
+		// TODO
+	}
+	else
+	{
+		while(++idx < table->set->total_philos)
+		{
+			safe_thread_handler(&table->philo->th_id[idx], routine, &table->philo[idx].id, CREATE);
+		}
+	}
+}
