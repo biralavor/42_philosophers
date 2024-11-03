07a4a19c566afa4607eefeb4f8860cfcbf534417
biralavor <umeneses@student.42sp.org.br>
2024-11-03 14:26:35 -0300

    removing unused table_holder function

--- philo/src/01.main.c
+++ philo/src/01.main.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 16:03:55 by umeneses          #+#    #+#             */
-/*   Updated: 2024/10/31 18:46:53 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/11/03 14:25:32 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,8 @@ int main(int ac, char **av)
 	if (!arguments_validation_manager(ac, av))
 		exit(EXIT_FAILURE);
 	table_parsing(&table, av);
-	// table = *table_holder(NULL, false);
 	table_init(&table);
 	dinner_manager(&table);
-	table_holder(NULL, true);
 	table_free(&table);
 	exit(EXIT_SUCCESS);
 }
