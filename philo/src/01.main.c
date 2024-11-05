971e4648633b4515ef0c46713b2f4634d410b263
biralavor <umeneses@student.42sp.org.br>
2024-11-05 11:15:55 -0300

    removing exit function

--- philo/src/01.main.c
+++ philo/src/01.main.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 16:03:55 by umeneses          #+#    #+#             */
-/*   Updated: 2024/11/04 18:50:31 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/11/05 11:14:01 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ int	main(int ac, char **av)
 	t_table	table;
 
 	if (!arguments_validation_manager(ac, av))
-		exit(EXIT_FAILURE);
+		return (EXIT_FAILURE);
 	table_parsing(&table, av);
 	table_init(&table);
 	dinner_manager(&table);
 	table_free(&table);
-	exit(EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
 }
