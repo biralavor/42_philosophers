a5d8d04afdb000e5b83f8f5343ee64f674f02d0a
biralavor <umeneses@student.42sp.org.br>
2024-11-10 10:58:56 -0300

    fixing sequence to let others philo run better

--- philo/src/12.dinner_routines.c
+++ philo/src/12.dinner_routines.c
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 16:19:23 by umeneses          #+#    #+#             */
-/*   Updated: 2024/11/09 20:03:35 by umeneses         ###   ########.fr       */
+/*   Updated: 2024/11/10 10:52:51 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,10 +31,10 @@ void	let_philo_eat_routine(t_philo *philo)
 	set_long(&philo->philo_mtx, &philo->time_of_last_meal,
 		ft_gettime(MILLISECOND));
 	printer_manager(EATING, philo, DEBUG_MODE);
+	precise_usleep(philo->table->set.time_to_eat, philo->table);
 	if (philo->table->set.total_meals > 0
 		&& philo->got_meals == philo->table->set.total_meals)
 		set_bool(&philo->philo_mtx, &philo->full, true);
-	precise_usleep(philo->table->set.time_to_eat, philo->table);
 	safe_mutex_handler(&philo->first_chops->chops_mtx, UNLOCK);
 	safe_mutex_handler(&philo->second_chops->chops_mtx, UNLOCK);
 }
